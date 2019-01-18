e257f5d5e9693eff44e01da4586394921c9f29aa
Wildan Kurniadi <danprogramming10@gmail.com>
2019-01-18 15:11:42 +0700

    "ForegroundService #1 Create app that Launch Service (normal service) "
    We Learn to create normal service here.
    in android API 26 or later, background service will be killed after about one minutes

---
+++ LearnForegroundService/app/src/main/java/com/wildanka/learnforegroundservice/ExampleService.java
@@ -0,0 +1,57 @@
+package com.wildanka.learnforegroundservice;
+
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.support.v4.app.NotificationCompat;
+
+import static com.wildanka.learnforegroundservice.App.CHANNEL_ID;
+
+public class ExampleService extends Service {
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    //this method will be triggered when we start our service
+//    be careful as onStartCommand is running on mainThread, so it can affect your app performance
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        String inString = intent.getStringExtra("inputExtra");
+
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
+
+        Notification notification = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setContentTitle("ExampleService")
+                .setContentText(inString)
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setAutoCancel(true)
+                .setContentIntent(pendingIntent)
+                .build();
+
+//        if we don't start our service in foreground, our service will be killed in about one minutes.
+//        for example, just try to comment startForeground line below and see on the running services via developer options menu in your android emulator/smartphone
+//        startForeground(1, notification);
+
+        return START_NOT_STICKY; //not sticky so this service will only create one service if there is a change or modification
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    //this is mandatory, but this is only for binding purpose so other components can communicate by binding, but the method that matters
+    // is onStartCommand()
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
--- LearnForegroundService/app/src/main/java/com/wildanka/learnforegroundservice/MainActivity.java
+++ LearnForegroundService/app/src/main/java/com/wildanka/learnforegroundservice/MainActivity.java
@@ -1,13 +1,32 @@
 package com.wildanka.learnforegroundservice;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText etInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etInput = (EditText) findViewById(R.id.et_input);
+    }
+
+    public void startService(View v) {
+        String input = etInput.getText().toString();
+        Intent serviceIntent = new Intent(this,ExampleService.class);
+        serviceIntent.putExtra( "inputExtra", input);
+
+        startService(serviceIntent);
+    }
+
+    public void stopService(View v) {
+        Intent serviceIntent = new Intent(this,ExampleService.class);
+        stopService(serviceIntent);
     }
 }
