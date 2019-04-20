e83bf4de984c565bd4aa229a7a7778b53d066eff
WildanKurniadi <danprogramming10@gmail.com>
2019-04-20 14:45:29 +0700

    Dagger DI #02 - Normal Dependencies example 1

--- Dagger2Introduction/app/src/main/java/com/wildanka/dagger2introduction/Car.java
+++ Dagger2Introduction/app/src/main/java/com/wildanka/dagger2introduction/Car.java
@@ -1,5 +1,15 @@
 package com.wildanka.dagger2introduction;
 
 public class Car {
+    Engine engine;
+    Wheels wheels;
 
+    public Car(Engine engine, Wheels wheels) {
+        this.engine = engine;
+        this.wheels = wheels;
+    }
+
+    void drive(){
+        //Vroom...
+    }
 }
--- Dagger2Introduction/app/src/main/java/com/wildanka/dagger2introduction/MainActivity.java
+++ Dagger2Introduction/app/src/main/java/com/wildanka/dagger2introduction/MainActivity.java
@@ -10,5 +10,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Engine engine = new Engine();
+        Wheels wheels = new Wheels();
+
+        /**
+         * Before using dagger we need to do quite a lot of initialization.
+         * Whereas before, when our car is created its own dependencies,
+         * we didn't have to do this stuff. because then we didn't have to pass them as arguments
+         * */
+        Car car = new Car(engine, wheels);
+
+        //then we can call car.drive
+        car.drive();
     }
 }
