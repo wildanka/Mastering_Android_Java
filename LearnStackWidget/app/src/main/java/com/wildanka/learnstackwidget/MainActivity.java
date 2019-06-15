90bafbfea167821586b5f6ef653cb157bf70db82
WildanKurniadi <danprogramming10@gmail.com>
2019-06-15 20:18:59 +0700

    StackWidget #06 - Update Widget every button clicked using JobService

--- LearnStackWidget/app/src/main/java/com/wildanka/learnstackwidget/MainActivity.java
+++ LearnStackWidget/app/src/main/java/com/wildanka/learnstackwidget/MainActivity.java
@@ -1,14 +1,69 @@
 package com.wildanka.learnstackwidget;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.job.JobInfo;
+import android.app.job.JobScheduler;
+import android.content.ComponentName;
+import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private Button btnStart;
+    private Button btnStop;
 
+    private static int jobId = 100;
+    private static int SCHEDULE_OF_PERIOD = 3000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnStart = findViewById(R.id.btn_start);
+        btnStop = findViewById(R.id.btn_stop);
+        btnStart.setOnClickListener(this);
+        btnStop.setOnClickListener(this);
     }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        switch (id) {
+            case R.id.btn_start:
+                startJob();
+                break;
+            case R.id.btn_stop:
+                cancelJob();
+                break;
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    private void startJob() {
+        ComponentName mServiceComponent = new ComponentName(this, UpdateWidgetService.class);
+        JobInfo.Builder builder = new JobInfo.Builder(jobId, mServiceComponent);
+        builder.setRequiredNetworkType(JobInfo.NETWORK_TYPE_NONE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            builder.setMinimumLatency(SCHEDULE_OF_PERIOD);
+        } else {
+            builder.setPeriodic(SCHEDULE_OF_PERIOD);
+        }
+        JobScheduler jobScheduler = (JobScheduler) getSystemService(Context.JOB_SCHEDULER_SERVICE);
+        jobScheduler.schedule(builder.build());
+        Toast.makeText(this, "Job Service started", Toast.LENGTH_SHORT).show();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    private void cancelJob() {
+        JobScheduler tm = (JobScheduler) getSystemService(Context.JOB_SCHEDULER_SERVICE);
+        tm.cancel(jobId);
+        Toast.makeText(this, "Job Service canceled", Toast.LENGTH_SHORT).show();
+    }
+
 }
---
+++ LearnStackWidget/app/src/main/java/com/wildanka/learnstackwidget/UpdateWidgetService.java
@@ -0,0 +1,34 @@
+package com.wildanka.learnstackwidget;
+
+import android.annotation.TargetApi;
+import android.app.job.JobParameters;
+import android.app.job.JobService;
+import android.appwidget.AppWidgetManager;
+import android.content.ComponentName;
+import android.os.Build;
+import android.widget.RemoteViews;
+
+import com.wildanka.learnstackwidget.SimpleWidget.NumberGenerator;
+import com.wildanka.learnstackwidget.SimpleWidget.RandomNumbersWidget;
+
+import androidx.annotation.RequiresApi;
+
+@RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+public class UpdateWidgetService extends JobService {
+    @Override
+    public boolean onStartJob(JobParameters params) {
+        AppWidgetManager manager = AppWidgetManager.getInstance(this);
+        RemoteViews view = new RemoteViews(getPackageName(), R.layout.random_numbers_widget);
+        ComponentName theWidget = new ComponentName(this, RandomNumbersWidget.class);
+        String lastUpdate = "Random: " + NumberGenerator.Generate(100);
+        view.setTextViewText(R.id.appwidget_text, lastUpdate);
+        manager.updateAppWidget(theWidget, view);
+        jobFinished(params, false);
+        return true;
+    }
+
+    @Override
+    public boolean onStopJob(JobParameters params) {
+        return false;
+    }
+}
