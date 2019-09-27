817c443cdbd54c6279dac0da15eb154b7f4a841a
WildanKurniadi <danprogramming10@gmail.com>
2019-09-27 16:55:21 +0700

    MonthYearPicker #3 - Bug Fixing

--- MonthYearPicker/app/src/main/java/com/wildanka/monthyearpicker/MainActivity.java
+++ MonthYearPicker/app/src/main/java/com/wildanka/monthyearpicker/MainActivity.java
@@ -3,6 +3,7 @@ package com.wildanka.monthyearpicker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.NumberPicker;
 import android.widget.TextView;
 
@@ -18,8 +19,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Locale locale = new Locale("in");
-        NumberPicker npMonth = findViewById(R.id.np_month);
-        NumberPicker npYear = findViewById(R.id.np_year);
+        final NumberPicker npMonth = findViewById(R.id.np_month);
+        final NumberPicker npYear = findViewById(R.id.np_year);
 
         npYear.setMinValue(1900);
         npYear.setMaxValue(2100);
@@ -32,15 +33,18 @@ public class MainActivity extends AppCompatActivity {
         npMonth.setFormatter(new NumberPicker.Formatter() {
             @Override
             public String format(int value) {
+                Log.e("TAG", "format: "+months[value] );
                 return months[value];
             }
         });
 
         int year = Calendar.getInstance().get(Calendar.YEAR); //get default year
         int month = Calendar.getInstance().get(Calendar.MONTH); //get default month
-
         npYear.setValue(year);
-        npMonth.setValue(month);
+        npMonth.setDisplayedValues(months); //set the displayed value first
+        npMonth.setValue(month); // then set defaultValues
 
+//        npMonth.setDescendantFocusability(NumberPicker.FOCUS_BLOCK_DESCENDANTS);
+//        npYear.setDescendantFocusability(NumberPicker.FOCUS_BLOCK_DESCENDANTS);
     }
 }
