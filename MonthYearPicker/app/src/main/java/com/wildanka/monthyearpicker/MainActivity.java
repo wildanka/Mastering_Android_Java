2c8b16fa18e69640dadf4a5624959245adf01b8d
WildanKurniadi <danprogramming10@gmail.com>
2019-09-27 15:47:57 +0700

    MonthYearPicker #2 - month & year displayed

--- MonthYearPicker/app/src/main/java/com/wildanka/monthyearpicker/MainActivity.java
+++ MonthYearPicker/app/src/main/java/com/wildanka/monthyearpicker/MainActivity.java
@@ -3,6 +3,12 @@ package com.wildanka.monthyearpicker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.NumberPicker;
+import android.widget.TextView;
+
+import java.text.DateFormatSymbols;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Locale locale = new Locale("in");
+        NumberPicker npMonth = findViewById(R.id.np_month);
+        NumberPicker npYear = findViewById(R.id.np_year);
+
+        npYear.setMinValue(1900);
+        npYear.setMaxValue(2100);
+
+        npMonth.setMinValue(0); // January
+        npMonth.setMaxValue(11); // December
+
+        //formatter
+        final String[] months = DateFormatSymbols.getInstance(locale).getShortMonths();
+        npMonth.setFormatter(new NumberPicker.Formatter() {
+            @Override
+            public String format(int value) {
+                return months[value];
+            }
+        });
+
+        int year = Calendar.getInstance().get(Calendar.YEAR); //get default year
+        int month = Calendar.getInstance().get(Calendar.MONTH); //get default month
+
+        npYear.setValue(year);
+        npMonth.setValue(month);
+
     }
 }
